8a846f43b8f7e4791c595f3ceabaacbf3a62a49d
MizouziE <90829439+MizouziE@users.noreply.github.com>
2021-11-16 12:12:22 +0000

    services CRUD module generated with mig, mod, repo, cont, frmRq, frmV needing updating

---
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use A17\Twill\Http\Controllers\Admin\ModuleController;
+
+class ServiceController extends ModuleController
+{
+    protected $moduleName = 'services';
+    
+}
